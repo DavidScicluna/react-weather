57e4a982565a2057d3b30a8315bc7b26464d2e6c
DavidScicluna\DavidScicluna <david.scicluna.a106106@mcast.edu.mt>
2019-12-23 05:54:22 +0100

    Updates

---
+++ precache-manifest.9697de84527780fde2a9c2fc50eb75ee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8640fc4060a42db18e6bc16425127ef5",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "d0d8a885bf2c436d2178",
+    "url": "/react-weather/static/css/main.7cda7c34.chunk.css"
+  },
+  {
+    "revision": "a16d88d7c8da7147cde4",
+    "url": "/react-weather/static/js/2.eb9d2d38.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-weather/static/js/2.eb9d2d38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0d8a885bf2c436d2178",
+    "url": "/react-weather/static/js/main.d234b163.chunk.js"
+  },
+  {
+    "revision": "f48129b7e11095b525ed",
+    "url": "/react-weather/static/js/runtime-main.1743bd74.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
